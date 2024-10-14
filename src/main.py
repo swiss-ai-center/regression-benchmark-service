50608e60153078ccbd0bf6e4df2d26f083674c82
Andrea Petrucci <andrea.petrucci@hefr.ch>
2024-10-14 11:44:35 +0200

    changed summary

--- src/main.py
+++ src/main.py
@@ -159,9 +159,6 @@ Also to improve the results you may want to remove uneccessary columns from the 
 Finally, avoid having multiple empty lines at the end of the file.
 """
 api_summary = """This service benchmarks a dataset with various models and outputs the results sorted by accuracy.
-In order for the service to work your dataset label column must be called "target".
-Also to improve the results you may want to remove uneccessary columns from the dataset.
-Finally, avoid having multiple empty lines at the end of the file.
 """
 
 # Define the FastAPI application with information
